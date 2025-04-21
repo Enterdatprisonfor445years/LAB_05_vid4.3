04782a39cca31b5cc81a50dc19f214fb6231703f
Enterdatprisonfor445years <85706754+Enterdatprisonfor445years@users.noreply.github.com>
2025-04-21 03:38:55 -0400

    4.7 Checking The Combination
    Added logic to check and validate combination inputs.

--- GameDev_VR_Course/Assets/Scripts/CombinationLock.cs
+++ GameDev_VR_Course/Assets/Scripts/CombinationLock.cs
@@ -4,15 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class CombinationLock : MonoBehaviour
 {
     [SerializeField] TMP_Text userInputText;
     [SerializeField] XrButtonInteractable[] comboButtons;
+    [SerializeField] Image lockedPanel;
+    [SerializeField] Color unlockedColor;
+    [SerializeField] TMP_Text lockedText;
+    private const string unlockedString = "unlocked";
+    [SerializeField] bool isLocked;
+    [SerializeField] int[] comboValues = new int[3];
+    [SerializeField] int[] inputValues;
+    private int maxButtonPresses;
+    private int buttonPresses;
     void Start()
     {
-        userInputText.text = "";
+        maxButtonPresses = comboValues.Length;
+        ResetUserValues();
         for (int i = 0; i < comboButtons.Length; i++)
         {
             comboButtons[i].selectEntered.AddListener(OnComboButtonPressed);
@@ -21,16 +32,58 @@ public class CombinationLock : MonoBehaviour
 
     private void OnComboButtonPressed(SelectEnterEventArgs arg0)
     {
-        for (int i = 0; i < comboButtons.Length; i++)
+        if (buttonPresses >= maxButtonPresses)
         {
-            if (arg0.interactableObject.transform.name == comboButtons[i].transform.name)
+
+        }
+        else
+        {
+            for (int i = 0; i < comboButtons.Length; i++)
             {
-                userInputText.text = i.ToString();
+                if (arg0.interactableObject.transform.name == comboButtons[i].transform.name)
+                {
+                    userInputText.text = i.ToString();
+                    inputValues[buttonPresses] = i;
+                }
+                else
+                {
+                    comboButtons[i].ResetColor();
+                }
             }
-            else
+            buttonPresses++;
+            if (buttonPresses == maxButtonPresses)
             {
-                comboButtons[i].ResetColor();
+                checkCombo();
             }
         }
+            
+    }
+    private void checkCombo()
+    {
+        int matches = 0;
+
+        for (int i = 0; i < maxButtonPresses; i++)
+        {
+            if (inputValues[i] == comboValues[i])
+            {
+                matches++;
+            }
+        }
+        if (matches == maxButtonPresses)
+        {
+            isLocked = false;
+            lockedPanel.color = unlockedColor;
+            lockedText.text = unlockedString;
+        }
+        else
+        {
+            ResetUserValues();
+        }
+    }
+    private void ResetUserValues()
+    {
+        inputValues = new int[maxButtonPresses];
+        userInputText.text = "";
+        buttonPresses = 0;
     }
 }
